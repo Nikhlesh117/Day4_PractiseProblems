9a03e734ec878fe6aacc9f4ac9689a6c7ab307f9
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-03 18:59:20 +0530

    Added: Number Of Duplicate Elements

---
+++ Array/Array/DuplicateElements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public class DuplicateElements
+    {
+        public void NoOfDuplicateElements() 
+        {
+            int[] arr = { 1, 2, 3, 2, 3, 4, 5, 4, 6, 7, 8, 9, 9, 10 };
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Total number of duplicate elements in the array: " + count);
+        }
+    }
+}
---
+++ Array/Array/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DuplicateElements duplicate = new DuplicateElements();
+            duplicate.NoOfDuplicateElements();
+        }
+    }
+}
